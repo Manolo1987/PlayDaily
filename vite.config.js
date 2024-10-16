d919a813b0924804b360523fcd0bbe52da30fcf5
manolo1987 <m.zeh.mz@gmail.com>
2024-10-16 10:18:49 +0200

    adding polyfills to vite.config.js for allwoing process to work and define buffer and fixing the according error - adding google.api script to inddex.html - resizing img in navbar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from "vite";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      nodePolyfills(), // this is necessary to avoid "process is not defined issue"
+      react(),
+    ],
+  };
+});
