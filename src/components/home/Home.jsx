f2db747779224b945ff330f3c19deeb949a1ffa7
manolo1987 <m.zeh.mz@gmail.com>
2024-11-05 10:33:41 +0100

    fixing api import for vite & crossahir.png in navbar

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,39 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Home.module.css';
 import { NavLink } from 'react-router-dom'
-import dotenv from 'dotenv';
-// dotenv.config();
-dotenv.config({ path: '../.env' });
 
-const apiKey = process.env.API_KEY;
+const apiKey = import.meta.env.VITE_API_KEY;
+// bei VITE projeckten muss .env so importiert werden, anders geht es nicht
 
 export default function Home() {
-  console.log(apiKey);
-  
-  console.log(process.env.API_KEY);
+
   const [data, setData] = useState([]);
   const [topArticle, setTopArticle] = useState([]);
-  
+
   const russianAlphabet = [
     'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й',
     'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
     'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'
   ];
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         // keine good practice mit dem hardgecodeten API key, aber wenigstens nicht direkt sichtbar
         // soll sonst verschlüsselt im backend auf dem server liegen und nur so oft wie nötig benutzt und übermittelt werden, da es ein sicherheitsrisiko ist
-        
-        
+
+
         // mit hardgecodetem API key funktioniert es
-        
-        console.log(process.env.API_KEY);
+
         const response = await fetch(`https://gnews.io/api/v4/search?q=cs2&apikey=${apiKey}`);
         const data = await response.json();
-        console.log(data);
-        
+        // console.log(data);
+
 
         // filtert die Artiekl heraus die russische Schriftzeichen enthalten
         const filteredArticles = data.articles.filter(article => {
@@ -54,8 +49,8 @@ export default function Home() {
           console.warn('No articles found in English. Sorry.');
         } else {
           setData(filteredArticles);
-          console.log(filteredArticles);
-          console.log(filteredArticles[0]); // checken welcher teil gebraucht wird
+          // console.log(filteredArticles);
+          // console.log(filteredArticles[0]); // checken welcher teil gebraucht wird
           setTopArticle(filteredArticles[0]);
         }
       } catch (error) {
@@ -70,20 +65,22 @@ export default function Home() {
     <div className={styles.home}>
       <h2>Top News</h2>
       <div className={styles.heroWrapper}>
-      {data.slice(0, 2).map((article, index) => (
-          <div className={styles.hero}>
+        {data.slice(0, 2).map((article, index) => (
+          <div className={styles.hero} key={index}>
+            {/* Mit MongoDB objectID nutzen für den key in Zeile 69 */}
             <a href={article.url} target="_blank" rel="noopener noreferrer" key={index}>
               <h4>{article.title}</h4>
               <img src={article.image} alt="top article image" />
             </a>
           </div>
-      ))}
+        ))}
       </div>
       <div className={styles.progress}>
-      <NavLink to="/stats">
-       <button className={styles.btn}>Track Progress</button>
-      </NavLink>
+        <NavLink to="/stats">
+          <button className={styles.btn}>Track Progress</button>
+        </NavLink>
       </div>
     </div>
   );
 }
+
