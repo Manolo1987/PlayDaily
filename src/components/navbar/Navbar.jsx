e36ab43d34c5a9fe2eab88b66064ad90da22b434
manolo1987 <m.zeh.mz@gmail.com>
2024-11-05 10:33:41 +0100

    fixing api import for vite & crossahir.png in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './Navbar.module.css'
-import Crosshair from '../../../public/crosshair.png'
+import Crosshair from 'crosshair.png'
 
 export default function navbar() {
   return (
